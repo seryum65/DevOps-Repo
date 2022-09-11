3924f9415943b1265bb5b1f24f6fe16ee7d91299
yusuf <cloudengineer.yusuf@gmail.com>
2022-09-11 16:57:53 +0300

    11.09.2022 updated

--- hands-on/ECR/ecr-01-elastic-container-registry-and-docker-images/ecr-ec2.tf
+++ hands-on/ECR/ecr-01-elastic-container-registry-and-docker-images/ecr-ec2.tf
@@ -101,5 +101,5 @@ output "ec2-public-ip" {
 }
 
 output "ssh-connection" {
-  value = "ssh -i ~/.ssh/davidskey.pem ec2-user@${aws_instance.ecr-instance.public_ip}"
+  value = "ssh -i ~/.ssh/firstkey.pem ec2-user@${aws_instance.ecr-instance.public_ip}"
 }
