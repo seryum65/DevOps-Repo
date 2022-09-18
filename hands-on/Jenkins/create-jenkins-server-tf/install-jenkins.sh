4ed1405d9747df57f285ef0603d3a4d78c5fbac1
yusuf <cloudengineer.yusuf@gmail.com>
2022-09-18 21:47:55 +0300

    18.09.2022 updated

--- hands-on/Jenkins/create-jenkins-server-tf/install-jenkins.sh
+++ hands-on/Jenkins/create-jenkins-server-tf/install-jenkins.sh
@@ -4,7 +4,7 @@ wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenki
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum upgrade
 amazon-linux-extras install java-openjdk11 -y
-amazon-linux-extras install epel -y
+#amazon-linux-extras install epel -y
 yum install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
