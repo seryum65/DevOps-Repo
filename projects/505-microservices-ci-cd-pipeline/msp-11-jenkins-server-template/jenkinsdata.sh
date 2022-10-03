9516d59a8562d238a72b6357ea8842b3e2ebe069
yusuf <cloudengineer.yusuf@gmail.com>
2022-10-03 11:39:55 +0300

    03.10.2022 updated

--- projects/505-microservices-ci-cd-pipeline/msp-11-jenkins-server-template/jenkinsdata.sh
+++ projects/505-microservices-ci-cd-pipeline/msp-11-jenkins-server-template/jenkinsdata.sh
@@ -21,12 +21,6 @@ systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
 usermod -a -G docker jenkins
-# configure docker as cloud agent for jenkins
-cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
-sed -i 's/^ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2375 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
-systemctl daemon-reload
-systemctl restart docker
-systemctl restart jenkins
 # install docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
